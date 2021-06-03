9ddafb622a87b0f7a584f4a82cf577f0ef26e2bc
Faysal Shuvo <32492440+FaysalShuvo@users.noreply.github.com>
2021-06-03 20:45:56 +0600

    added input

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Input from "./components/Input/Input";
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List with Redux</h1>
+      <Input />
     </div>
   );
 }
